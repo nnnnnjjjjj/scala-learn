866a1c1436949a8d0191ad819d0f4db4a675870c
zyt <zyt@example.com>
2022-10-31 23:59:51 +0800

    update

--- src/main/scala/chapter01/HelloWorld.scala
+++ src/main/scala/chapter01/HelloWorld.scala
@@ -6,8 +6,26 @@ package chapter01
 object HelloWorld {
   def main(args: Array[String]): Unit = {
 
-    var name = "zhangsan"
-    var age = 15
-    println(s"name=$name, age=$age")
+    var arr: Array[int] = new Array[Any](5)
+
+    var arr2 = Array(2, 3, 42, 21, 3)
+
+    for (i <- 0 until arr2.length) {
+      println(arr2(i))
+    }
+
+    for (i <- arr2.indices) {
+      println(arr2(i))
+    }
+
+    for (elem <- arr2) {
+      println(elem)
+    }
+
+    var iter = arr2.iterator
+    while (iter.hasNext)
+      println(iter.next())
+
+    arr2.foreach(println)
   }
 }
